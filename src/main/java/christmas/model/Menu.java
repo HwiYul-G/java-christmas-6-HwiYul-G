ef3ccf1e63b93bc2fbea3901498b8ddd88866bc3
kim <hwiyul_g@daum.net>
2023-11-09 23:28:57 +0900

    feat(Menu) : 메뉴 카테고리와 아이템을 이넘으로 추가

---
+++ src/main/java/christmas/model/Menu.java
@@ -0,0 +1,42 @@
+package christmas.model;
+
+public enum Menu {
+    MUSHROOM_SOUP(6000, Category.APPETIZER),
+    TAPAS(5500, Category.APPETIZER),
+    CAESAR_SALAD(8000, Category.APPETIZER),
+
+
+    T_BONE_STEAK(55000, Category.MAIN),
+    BBQ_RIBS(54000, Category.MAIN),
+    SEAFOOD_PASTA(35000, Category.MAIN),
+    CHRISTMAS_PASTA(25000, Category.MAIN),
+
+
+    CHOCOLATE_CAKE(15000, Category.DESSERT),
+    ICE_CREAM(5000, Category.DESSERT),
+
+
+    ZERO_COLA(3000, Category.BEVERAGE),
+    RED_WINE(60000, Category.BEVERAGE),
+    CHAMPAGNE(25000, Category.BEVERAGE);
+
+    private final int price;
+    private final Category category;
+
+    Menu(final int price, final Category category) {
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public enum Category{
+        APPETIZER, MAIN, DESSERT, BEVERAGE
+    }
+}
