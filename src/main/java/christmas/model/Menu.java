64b150ac1f9cc50a6b0775762d233e2d7b181aaf
kim <hwiyul_g@daum.net>
2023-11-11 15:34:59 +0900

    refactor : 가독성 향상을 위해 숫자에 언더바 추가

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,24 +1,24 @@
 package christmas.model;
 
 public enum Menu {
-    MUSHROOM_SOUP(6000, Category.APPETIZER),
-    TAPAS(5500, Category.APPETIZER),
-    CAESAR_SALAD(8000, Category.APPETIZER),
+    MUSHROOM_SOUP(6_000, Category.APPETIZER),
+    TAPAS(5_500, Category.APPETIZER),
+    CAESAR_SALAD(8_000, Category.APPETIZER),
 
 
-    T_BONE_STEAK(55000, Category.MAIN),
-    BBQ_RIBS(54000, Category.MAIN),
-    SEAFOOD_PASTA(35000, Category.MAIN),
-    CHRISTMAS_PASTA(25000, Category.MAIN),
+    T_BONE_STEAK(55_000, Category.MAIN),
+    BBQ_RIBS(54_000, Category.MAIN),
+    SEAFOOD_PASTA(35_000, Category.MAIN),
+    CHRISTMAS_PASTA(25_000, Category.MAIN),
 
 
-    CHOCOLATE_CAKE(15000, Category.DESSERT),
-    ICE_CREAM(5000, Category.DESSERT),
+    CHOCOLATE_CAKE(15_000, Category.DESSERT),
+    ICE_CREAM(5_000, Category.DESSERT),
 
 
-    ZERO_COLA(3000, Category.BEVERAGE),
-    RED_WINE(60000, Category.BEVERAGE),
-    CHAMPAGNE(25000, Category.BEVERAGE);
+    ZERO_COLA(3_000, Category.BEVERAGE),
+    RED_WINE(60_000, Category.BEVERAGE),
+    CHAMPAGNE(25_000, Category.BEVERAGE);
 
     private final int price;
     private final Category category;
