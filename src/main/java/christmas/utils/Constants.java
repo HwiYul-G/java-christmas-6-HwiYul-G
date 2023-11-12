4f23cafb15f568c0ca63d8b6bc77bf238571a198
kim <hwiyul_g@daum.net>
2023-11-12 20:35:06 +0900

    feat(Constants) : 필요한 상수 추가

--- src/main/java/christmas/utils/Constants.java
+++ src/main/java/christmas/utils/Constants.java
@@ -5,10 +5,12 @@ public class Constants {
     public static final int LAST_DATE = 31;
 
     public static final int MINIMUM_ORDER_QUANTITY = 1;
-    public static final int MAXIMUM_ORDER_QUANTITY = 20;
+    public static final int MAXIMUM_TOTAL_ORDER_QUANTITY = 20;
 
     public static final int DAY_OF_WEEK_DISCOUNT_AMOUNT = 2_023;
     public static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
     public static final int NO_DISCOUNT = 0;
     public static final int SHAMPAGNE_EVENT_THRESHOLD_PRICE = 120_000;
+
+    public static final int EVENT_ELIGIBILITY_THRESHOLD_PRICE = 10_000;
 }
