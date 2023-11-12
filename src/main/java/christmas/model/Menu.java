f8b57c50293cd0e0bb662e806c8195077cf9e628
kim <hwiyul_g@daum.net>
2023-11-12 17:32:33 +0900

    feat(Menu) : 메뉴판 생성 및 string 기반으로 메뉴 검색 함수 추가

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,35 +1,43 @@
 package christmas.model;
 
+import java.util.Arrays;
+
 public enum Menu {
-    MUSHROOM_SOUP(6_000, Category.APPETIZER),
-    TAPAS(5_500, Category.APPETIZER),
-    CAESAR_SALAD(8_000, Category.APPETIZER),
+    MUSHROOM_SOUP(6_000, Category.APPETIZER, "양송이수프"),
+    TAPAS(5_500, Category.APPETIZER, "타파스"),
+    CAESAR_SALAD(8_000, Category.APPETIZER, "시저샐러드"),
 
 
-    T_BONE_STEAK(55_000, Category.MAIN),
-    BBQ_RIBS(54_000, Category.MAIN),
-    SEAFOOD_PASTA(35_000, Category.MAIN),
-    CHRISTMAS_PASTA(25_000, Category.MAIN),
+    T_BONE_STEAK(55_000, Category.MAIN, "티본스테이크"),
+    BBQ_RIBS(54_000, Category.MAIN, "바비큐립"),
+    SEAFOOD_PASTA(35_000, Category.MAIN, "해산물파스타"),
+    CHRISTMAS_PASTA(25_000, Category.MAIN, "크리스마스파스타"),
 
 
-    CHOCOLATE_CAKE(15_000, Category.DESSERT),
-    ICE_CREAM(5_000, Category.DESSERT),
+    CHOCOLATE_CAKE(15_000, Category.DESSERT, "초코케이크"),
+    ICE_CREAM(5_000, Category.DESSERT, "아이스크림"),
 
 
-    ZERO_COLA(3_000, Category.BEVERAGE),
-    RED_WINE(60_000, Category.BEVERAGE),
-    CHAMPAGNE(25_000, Category.BEVERAGE);
+    ZERO_COLA(3_000, Category.BEVERAGE, "제로콜라"),
+    RED_WINE(60_000, Category.BEVERAGE, "레드와인"),
+    CHAMPAGNE(25_000, Category.BEVERAGE, "샴페인");
 
     private final int price;
     private final Category category;
+    private final String foodName;
 
-    Menu(final int price, final Category category) {
+    Menu(final int price, final Category category, final String foodName) {
         this.price = price;
         this.category = category;
+        this.foodName = foodName;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    public String getFoodName() {
+        return foodName;
     }
 
     public boolean isBeverage() {
@@ -43,4 +51,12 @@ public enum Menu {
     public enum Category {
         APPETIZER, MAIN, DESSERT, BEVERAGE
     }
+
+    public static Menu getMenuByName(String foodName) {
+        return Arrays.stream(Menu.values())
+            .filter(menu -> menu.getFoodName().equals(foodName))
+            .findFirst()
+            .orElseThrow(IllegalStateException::new);
+    }
+
 }
