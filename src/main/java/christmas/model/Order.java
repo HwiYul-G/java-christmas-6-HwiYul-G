d0a658771a3d0ef92e75640b6890031bcd1d1b73
kim <hwiyul_g@daum.net>
2023-11-28 14:12:18 +0900

    feat(Order) : Order에 toOrderItems 추가

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,7 +1,9 @@
 package christmas.model;
 
 import christmas.model.data.Menu;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record Order(Map<Menu, Integer> orderItems) {
     private static final int EVENT_THRESHOLD_PRICE = 10_000;
@@ -16,5 +18,11 @@ public record Order(Map<Menu, Integer> orderItems) {
         return calculateTotalOrderAmount() >= EVENT_THRESHOLD_PRICE;
     }
 
+    public OrderItems toOrderItems(){
+        List<OrderItem> items = orderItems.entrySet().stream()
+            .map(entry -> new OrderItem(entry.getKey().getFoodName(), entry.getValue()))
+            .collect(Collectors.toList());
 
+        return new OrderItems(items);
+    }
 }
