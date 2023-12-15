d0db0dced8efad59362fc5d5f0acb15e9407df42
kim <hwiyul_g@daum.net>
2023-12-15 22:02:07 +0900

    feat : inputView 작성

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,10 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Orders;
+import christmas.domain.VisitDate;
+import christmas.util.InputValidator;
+
 public class InputView {
 
     private static final InputView instance = new InputView();
@@ -9,11 +14,34 @@ public class InputView {
     }
 
     private InputView() {
-
     }
 
+    public int inputVisitDate() {
+        try {
+            System.out.printf(Message.INPUT_VISIT_DATE.message);
+            String input = Console.readLine();
+            return VisitDate.of(input).visitDate();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputVisitDate();
+        }
+    }
+
+    public Orders inputOrders() {
+        try {
+            System.out.printf(Message.INPUT_ORDER_MENU.message);
+            String input = Console.readLine();
+            return Orders.of(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputOrders();
+        }
+    }
+
+
     private enum Message {
-        INPUT_("");
+        INPUT_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n"),
+        INPUT_ORDER_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n");
         private final String message;
 
         Message(final String message) {
