ac0c7c0fc8bb88ca739374968e59c1235646b9af
kim <hwiyul_g@daum.net>
2023-12-15 22:01:44 +0900

    feat : 오류 메시지 추가 작성

--- src/main/java/christmas/util/ExceptionMessage.java
+++ src/main/java/christmas/util/ExceptionMessage.java
@@ -1,7 +1,14 @@
 package christmas.util;
 
 public enum ExceptionMessage {
-    INVALID_CATEGORY("존재하지 않는 카테고리입니다.");
+    INVALID_CATEGORY("존재하지 않는 카테고리입니다."),
+    INVALID_MENU("존재하지 않는 메뉴입니다."),
+    INVALID_VISITDATE("방문 날짜는 숫자만 입력 가능합니다."),
+    INVALID_ORDERS_QUANTITY("주문 수량은 20개를 초과할 수 없습니다."),
+    INVALID_VISITDATE_NUMBER("방문 날짜는 숫자만 입력 가능합니다."),
+    INVALID_VISITDATE_MIN_RANGE("방문 날짜는 1일 이상만 가능합니다."),
+    INVALID_VISITDATE_MAX_RANGE("방문 날짜는 31일 이하만 가능합니다."),
+    INVALID_ORDER_QUANTITY("메뉴당 주문 수량은 1 이상만 가능합니다."), INVALID_ORDERS_DUPLICATION("입력시 메뉴를 중복되게 입력할 수 없습니다.");
 
     public static final String BASE_MESSAGE = "[ERROR] : %s";
     private final String message;
