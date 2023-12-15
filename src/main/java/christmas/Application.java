128eabdb39c83e561a5779235b0a4e9dc0679b51
kim <hwiyul_g@daum.net>
2023-12-15 14:40:40 +0900

    init : 처음부터 시작하기 위해 초기화

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,22 +1,8 @@
 package christmas;
 
-import christmas.service.CalculatorService;
-import christmas.controller.InputController;
-import christmas.controller.OutputController;
-import christmas.view.InputView;
-import christmas.view.OutputView;
-
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        CalculatorService calculatorService = new CalculatorService();
 
-        InputController inputController = new InputController(inputView, calculatorService);
-        OutputController outputController = new OutputController(outputView, calculatorService);
-
-        inputController.run();
-        outputController.run();
     }
 }
